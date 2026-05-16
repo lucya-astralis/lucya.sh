a82389781d6f27d0f088135d4854ed48b6085ed9
astra <astra@inter-astra.net>
2026-05-16 13:00:02 +0200

    neues profil wallpaper

--- script.js
+++ script.js
@@ -812,14 +812,14 @@
   if (spotifyCard) {
     const songs = [
       { title: "DEAD CENTER", artist: "REOL, LiSA, Giga", cover: "images/spotify_widget/dead_center.jpg" },
+      { title: "Rebirth", artist: "SHIMA", cover: "images/spotify_widget/rebirth.jpeg" },      
       { title: "Heaven", artist: "Allison Wonderland, Ninajirachi", cover: "images/spotify_widget/cover1.png" },
       { title: "Infohazard", artist: "Ninajirachi", cover: "images/spotify_widget/cover4.png" },
       { title: "H.E.R", artist: "Tearz", cover: "images/spotify_widget/H.E.R.jpg" },
       { title: "Vorozhyla", artist: "Korolova, Rokston, Go_A, Monokate", cover: "images/spotify_widget/Vorozhyla.jpg" },
       { title: "Elevate", artist: "Sub Focus", cover: "images/spotify_widget/cover3.jpeg" },
       { title: "Battery Death", artist: "Ninajirachi", cover: "images/spotify_widget/cover4.png" },
-      { title: "Ghostlight", artist: "Skeler, Veela", cover: "images/spotify_widget/Ghostlight.jpeg" },
-      { title: "Orbit", artist: "Koven, HALIENE", cover: "images/spotify_widget/cover5.jpg" }
+      { title: "Ghostlight", artist: "Skeler, Veela", cover: "images/spotify_widget/Ghostlight.jpeg" }
     ];
     const INTERVAL_MS = 5000;
     const cover = document.getElementById('spotifyCover');
